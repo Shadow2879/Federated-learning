9ea6336adffc93a72562db3efea72b1872ef74e8
Shadow2879 <anirudhkaushik712@gmail.com>
2025-05-31 23:05:24 +0000

    added a scheduler to handle running functions in the background. updated requirements in order to include relevant libs.

--- server/main.py
+++ server/main.py
@@ -11,20 +11,28 @@ from model_classes import model,agg_models
 import agg_strats
 from common.env_path_fns import load_env_var
 from datetime import datetime,timedelta
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.executors.pool import ThreadPoolExecutor
+from fastapi import FastAPI
+import uvicorn
+from contextlib import asynccontextmanager
 
 model_ver=0
 g_model=model()
 UPLOAD_MODEL_DIR=load_env_var('AGG_SERVER_UPLOAD_MODEL_DIR','path')
 RUNS=load_env_var('AGG_SERVER_RUNS','int')
 UPDATE_FREQ=load_env_var('AGG_SERVER_UPDATE_FREQ','int')
-model_counts={i:agg_models(
+models={i:agg_models(
     dir=f'{os.path.join(UPLOAD_MODEL_DIR,str(i))}',
     agg_strat=agg_strats.fed_avg,)
       for i in range(RUNS)}
-model_counts[0].add_global_model(g_model)
+models[0].add_global_model(g_model)
 GLOBAL_MODEL_DIR=load_env_var('AGG_SERVER_GLOBAL_MODEL_DIR','path')
 gen_update_time=lambda :datetime.now()+timedelta(seconds=UPDATE_FREQ)
 g_update_time=gen_update_time()
+periodic_event_scheduler=BackgroundScheduler(executor=ThreadPoolExecutor)
+model_counts=[0 for _ in range(RUNS)]
+model_update_status=""
 
 gr.set_static_paths(paths=[
     os.path.join(os.getcwd(),GLOBAL_MODEL_DIR),
@@ -32,46 +40,59 @@ gr.set_static_paths(paths=[
 ])
 
 def update_model() ->str:
-    global g_update_time
+    global g_update_time,model_update_status
     time_to_update=g_update_time-datetime.now()
     if time_to_update<timedelta(seconds=0):
         updated=agg_weights()
         if updated:
-            return f'global model updated'
+            model_update_status=f'global model updated'
         else:
             g_update_time=gen_update_time()
-            return f'not enough models, waiting till {g_update_time} for update.'
+            model_update_status=f'not enough models, waiting till {g_update_time} for update.'
     else:
-        return f'global model to be updated in :{time_to_update.seconds} seconds.'
+        model_update_status=f'global model to be updated in :{time_to_update.seconds} seconds.'
     
 def get_model_weights() ->str:
+    global model_ver
     f_loc=os.path.join(GLOBAL_MODEL_DIR,f'{model_ver}.pth')
     torch.save(g_model.state_dict(),f_loc)
     print(f"current modelfile: {f_loc}")
     return f_loc
 
 def get_model_ver()-> int:
+    global model_ver
     print(f'model_version:{model_ver}')
     return model_ver
 
 def upload_model(model_loc:str,counts:int) -> None:
     print('uploaded data:',model_loc,counts)
     global model_ver
-    model_counts[model_ver].add_client_model(model_loc)
+    models[model_ver].add_client_model(model_loc)
 
 def agg_weights() -> bool:
     global model_ver
-    g_model,update=model_counts[model_ver].update()
+    g_model,update=models[model_ver].update()
     if update:
         model_ver+=1
-        model_counts[model_ver].add_global_model(g_model)
+        models[model_ver].add_global_model(g_model)
         return update
     return False
 
+def get_models() -> None:
+    global model_counts
+    model_counts=[len(models[i].models) for i in range(RUNS)]
+    # return len(models[get_model_ver()].models)
+
+periodic_event_scheduler.add_job(update_model,trigger='interval',seconds=1)
+periodic_event_scheduler.add_job(get_model_ver,trigger='interval',seconds=1)
+periodic_event_scheduler.add_job(get_model_weights,trigger='interval',seconds=1)
+periodic_event_scheduler.add_job(get_models,trigger='interval',seconds=1)
+
 with gr.Blocks() as demo:
-    gr.Number(value=get_model_ver,every=10,label='get_model_ver')
-    gr.File(value=get_model_weights,every=10,label='get_model_weights')
-    gr.Text(value=update_model,every=1,label='model update info')
+    gr.Number(value=lambda :model_ver,every=5,label='get_model_ver')
+    gr.File(value=get_model_weights,every=5,label='get_model_weights')
+    gr.Number(value=lambda x=3:model_counts[model_ver],every=1,label='models uploaded')
+    gr.Text(value=lambda x=3:model_update_status,every=1,label='model update info')
     new_model=gr.File(label='upload_model')
     steps=gr.Number(value=0)
     upload_submit_btn=gr.Button(value='Upload')
@@ -79,8 +100,17 @@ with gr.Blocks() as demo:
         upload_model,
         [new_model,steps]
     )
+#needed for scheduler to stop at the end.
+async def lifespan(app:FastAPI):
+    periodic_event_scheduler.start()
+    yield
+    periodic_event_scheduler.shutdown()
+app=FastAPI(lifespan=lifespan)
+app=gr.mount_gradio_app(app,demo,path='/')
+uvicorn.run(app,host='0.0.0.0',port=load_env_var('AGG_SERVER_PORT','int'))
+# demo.launch(
+#     server_name='0.0.0.0',
+#     server_port=load_env_var('AGG_SERVER_PORT','int'),
+#     )
 
-demo.launch(
-    server_name='0.0.0.0',
-    server_port=load_env_var('AGG_SERVER_PORT','int')
-    )
+# periodic_event_scheduler.start()
